6340c8890df59ed2e8a2dfd76e78bc571993ebe9
Sean McPherson <seanmcp25@gmail.com>
2018-07-04 15:07:49 -0400

    Rename method on MapSquareClass
    Turns out 'yield' is a protected word in Python

--- classes/MapSquareClass.py
+++ classes/MapSquareClass.py
@@ -26,14 +26,14 @@ class MapSquare:
         self.resource_count = random.randint(0, 5)
         self.resource_type = self.resources[self.type]
     
-    def yield(self):
+    def produce(self):
         if self.resource_count:
             self.resource_count -= 1
             return self.resource_type
         return None
 
-# new_square = MapSquare('plain')
-# print(new_square.type)
-# print(new_square.resource_count)
-# print(new_square.yield())
-# print(new_square.resource_count)
+square = MapSquare('plain')
+print(square.type)
+print(square.resource_count)
+print(square.produce())
+print(square.resource_count)
