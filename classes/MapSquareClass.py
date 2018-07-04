8a63426d3bdc24ef8974001fb1d971c8cab78003
Sean McPherson <seanmcp25@gmail.com>
2018-07-04 14:20:41 -0400

    Add variables and method to MapSquareClass

--- classes/MapSquareClass.py
+++ classes/MapSquareClass.py
@@ -1,3 +1,30 @@
+import random
+
+terrains = ('forest', 'plain', 'mountain', 'cave', 'lake')
+resources = {
+    'forest': 'wood',
+    'plain': 'worms',
+    'mountain': 'rocks',
+    'cave': 'gems',
+    'lake': 'fish'
+}
+
 class MapSquare:
-    def __init__(self, type):
-        self.type = type
+    def __init__(self, prev_terrain):
+        # New terrain has a 50% chance of being the same
+        # as preivous terrain
+        self.type = prev_terrain if random.randint(0, 1) is 1 else terrains[random.randint(0, len(terrains) - 1)]
+        self.resource_count = random.randint(0, 5)
+        self.resource_type = resources[self.type]
+    
+    def dig(self):
+        if self.resource_count:
+            self.resource_count -= 1
+            return self.resource_type
+        return None
+
+# new_square = MapSquare('plain')
+# print(new_square.type)
+# print(new_square.resource_count)
+# print(new_square.dig())
+# print(new_square.resource_count)
