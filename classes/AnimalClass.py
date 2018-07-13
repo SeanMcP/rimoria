6d84f774f826bb085a5bd8249f5698cd5a2cdd22
Sean McPherson <seanmcp25@gmail.com>
2018-07-13 16:46:44 -0400

    Add methods to Animal class

--- classes/AnimalClass.py
+++ classes/AnimalClass.py
@@ -1,19 +1,25 @@
 import random
-from utils.data import get_animal
+from utils.data import get_animal, find_animal_type
 from utils.print import new_line
 
 class Animal:
 
-    def __init__(self, animal_type='buffalo'):
-        self.data = get_animal(animal_type)
-        self.type = animal_type
+    def __init__(self, resource_type='plain'):
+        self.type = find_animal_type(resource_type)
+        self.data = get_animal(self.type)
         self.energy = self.data['energy']
         self.experience = self.data['experience']
+        self.is_angry = False
         self.resource = self.data['resource']
         self.speed = self.data['speed']
         self.status = 'alive'
         self.strength = self.data['strength']
     
+    def anger(self):
+        if self.is_angry == False:
+            self.is_angry = True
+            new_line(f'The {self.type} is getting angry!')
+
     def attack(self):
         roll = random.randint(0, 20) + self.speed
         if roll < 10:
@@ -21,11 +27,24 @@ class Animal:
             return None
         new_line(f'{self.type} attack hits!')
         return self.strength
+
+    def calm(self):
+        if self.is_angry == True:
+            self.is_angry = False
+            new_line(f'The {self.type} is calming down.')
     
     def damage(self, amount):
         self.energy -= amount
         new_line(f'** The {self.type} takes {amount} damage **')
+        self.anger()
         self.status_check()
+
+    def feed(self, amount):
+        if amount > 5:
+            self.energy += amount
+            self.calm()
+        else:
+            self.anger()
     
     def run(self):
         roll = random.randint(0, 20) + self.speed
